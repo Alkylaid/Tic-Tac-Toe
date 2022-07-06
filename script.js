f4db61fc031505553c89ad18081a064349ae7d69
Alkylaid <alkylaid@gmail.com>
2022-07-05 19:34:58 -0500

    Update basic functionality for game

--- script.js
+++ script.js
@@ -1,48 +1,53 @@
-const gameboard = (() => {
-    let board = [' ', ' ', ' ',
-                ' ', ' ', ' ',
-                ' ', ' ', ' ',]
+const board = (() => {
+    let gameBoard = [' ', ' ', ' ',
+        ' ', ' ', ' ',
+        ' ', ' ', ' ',]
 
     function updateBoard(index, mark) {
-        board[index] = mark;
+        gameBoard[index] = mark;
     }
-    
-    function checkMove(index) {
-        return (board[index] === ' ');
+
+    function getBoard(index) {
+        return gameBoard[index];
+    }
+
+
+    function checkValidMove(index) {
+        return (gameBoard[index] === ' ');
     }
 
     function checkStatus() {
-
-        if (board[0] === board[1] && board[0] === board[2] && board[0] != ' ') {
-            controller.declareWinner(controller.getCurrentPlayer());
+    
+        if (gameBoard[0] === gameBoard[1] && gameBoard[0] === gameBoard[2] && gameBoard[0] != ' ') {
+            return controller.getCurrentPlayer();
         }
-        if (board[3] === board[4] && board[3] === board[5] && board[3] != ' ') {
-            controller.declareWinner(controller.getCurrentPlayer());
+        if (gameBoard[3] === gameBoard[4] && gameBoard[3] === gameBoard[5] && gameBoard[3] != ' ') {
+            return controller.getCurrentPlayer();
         }
-        if (board[6] === board[7] && board[6] === board[8] && board[6] != ' ') {
-            controller.declareWinner(controller.getCurrentPlayer());
+        if (gameBoard[6] === gameBoard[7] && gameBoard[6] === gameBoard[8] && gameBoard[6] != ' ') {
+            return controller.getCurrentPlayer();
         }
         //check columns
-        if (board[0] === board[3] && board[0] === board[6] && board[0] != ' ') {
-            controller.declareWinner(controller.getCurrentPlayer());
+        if (gameBoard[0] === gameBoard[3] && gameBoard[0] === gameBoard[6] && gameBoard[0] != ' ') {
+            return controller.getCurrentPlayer();
         }
-        if (board[1] === board[4] && board[1] === board[7] && board[1] != ' ') {
-            controller.declareWinner(controller.getCurrentPlayer());
+        if (gameBoard[1] === gameBoard[4] && gameBoard[1] === gameBoard[7] && gameBoard[1] != ' ') {
+            return controller.getCurrentPlayer();
         }
-        if (board[2] === board[5] && board[2] === board[8] && board[2] != ' ') {
-            controller.declareWinner(controller.getCurrentPlayer());
+        if (gameBoard[2] === gameBoard[5] && gameBoard[2] === gameBoard[8] && gameBoard[2] != ' ') {
+            return controller.getCurrentPlayer();
         }
 
         //check diagonals
-        if (board[0] === board[4] && board[0] === board[8] && board[0] != ' ') {
-            controller.declareWinner(controller.getCurrentPlayer());
+        if (gameBoard[0] === gameBoard[4] && gameBoard[0] === gameBoard[8] && gameBoard[0] != ' ') {
+            return controller.getCurrentPlayer();
         }
-        if (board[2] === board[4] && board[2] === board[6] && board[2] != ' ') {
-            controller.declareWinner(controller.getCurrentPlayer());
+        if (gameBoard[2] === gameBoard[4] && gameBoard[2] === gameBoard[6] && gameBoard[2] != ' ') {
+            return controller.getCurrentPlayer();
         }
 
         if (controller.getTotalMoves() === 9) {
-            controller.declareWinner("Draw");
+            return "Draw";
         }
 
 
@@ -50,14 +55,15 @@ const gameboard = (() => {
     }
     return {
         updateBoard: updateBoard,
-        checkMove: checkMove,
+        checkValidMove: checkValidMove,
         checkStatus: checkStatus,
+        getBoard: getBoard,
     };
 })();
 
 const playerFactory = (mark) => {
 
-    return {mark};
+    return { mark };
 }
 
 const playerOne = playerFactory("X");
@@ -67,50 +73,54 @@ const playerTwo = playerFactory("O");
 const controller = (() => {
     let currentPlayer = playerOne;
     let totalMoves = 0;
+    const container = document.querySelector('.container');
+
     const squares = document.querySelectorAll(".grid-item");
 
-    function playGame() {
-    squares.forEach((square, index) => {
-        square.addEventListener('click', () => {
-        if(gameboard.checkMove(index)) {
-            playMove(square, index);}
-        else {
-            alert("Pick another move");
+    (function render() {
+        squares.forEach((square, index) => {
+            square.addEventListener('click', () => {
+                playMove(index);
+            })
         }
-    })});
-}
+        )
+    })();
 
-    function playMove(square, index) {
-        square.innerHTML = currentPlayer.mark;
-        gameboard.updateBoard(index, currentPlayer.mark);
-        totalMoves++;
-        gameboard.checkStatus();
-        currentPlayer === playerOne ? currentPlayer = playerTwo : currentPlayer = playerOne;}
-    
+    function playMove(index) {
+        if (board.checkValidMove(index)) {
+            board.updateBoard(index, currentPlayer.mark)
+            squares[index].innerHTML = currentPlayer.mark;
+            totalMoves++;
+            if (board.checkStatus() === currentPlayer || board.checkStatus() === "Draw") {
+                board.checkStatus() === currentPlayer ? declareWinner(currentPlayer) : declareWinner("Draw");
+            }
+            currentPlayer === playerOne ? currentPlayer = playerTwo : currentPlayer = playerOne;
+            
 
-
-    function declareWinner(winner) {
-        
-        if (winner === playerOne) {
-            alert("You won");
-        } else if (winner === playerTwo) {
-            alert("The computer won");
-        } else if (totalMoves === 9){
-            alert("It's a draw!");
         }
-    }
 
-    function getCurrentPlayer() {
-        return currentPlayer;
     }
 
     function getTotalMoves() {
         return totalMoves;
     }
 
-    playGame();
-    return {getCurrentPlayer: getCurrentPlayer, getTotalMoves: getTotalMoves, declareWinner: declareWinner}
+    function declareWinner(winner) {
+        const score = document.createElement('div');
+        score.setAttribute('id', 'score');
+        if (winner === "Draw") {
+            score.innerHTML = "DRAW!";
+        } else {
+            score.innerHTML = `The winner is ${currentPlayer.mark}`
+        }
+        container.prepend(score);
+    }
 
+    function getCurrentPlayer() {
+        return currentPlayer;
+    }
+
+    return {getTotalMoves, getCurrentPlayer }
 })();
 
 
